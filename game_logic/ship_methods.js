a3cd56942b17f919ec24415c5cafc3449abaca9f
Alex Harri Jónsson <alexharri2919@gmail.com>
2018-03-07 14:18:53 +0000

    Adding the actual game logic from treehouse.

--- game_logic/ship_methods.js
+++ game_logic/ship_methods.js
@@ -1,7 +1,24 @@
-exports.checkForShip = (player, coordinates) => {
-  return true;
+function checkForShip (player, coordinates) {
+	var shipPresent, ship;
+	
+	for (var i = 0; i < player.ships.length; i++) {
+		ship = player.ships[i];
+		
+		shipPresent = ship.locations.filter(function (actualCoordinate) {
+			return (actualCoordinate[0] === coordinates[0]) && (actualCoordinate[1] === coordinates[1]);
+		})[0];
+		
+		if (shipPresent) {
+			return true;
+		} 
+	}
+	
+	return false;
 }
 
-exports.damageShip = (ship, coordinates) => {
-  
+function damageShip (ship, coordinates) {
+	ship.damage.push(coordinates);
 }
+
+module.exports.checkForShip = checkForShip;
+module.exports.damageShip = damageShip;
