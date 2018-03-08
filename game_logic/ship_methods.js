09547305e0d6948806f1e8cf7f6b0bdbd824dbc4
Alex Harri Jónsson <alexharri2919@gmail.com>
2018-03-08 12:51:53 +0000

    Adding the linter to the build process.

--- game_logic/ship_methods.js
+++ game_logic/ship_methods.js
@@ -1,24 +1,22 @@
-function checkForShip (player, coordinates) {
-	var shipPresent, ship;
-	
-	for (var i = 0; i < player.ships.length; i++) {
-		ship = player.ships[i];
-		
-		shipPresent = ship.locations.filter(function (actualCoordinate) {
-			return (actualCoordinate[0] === coordinates[0]) && (actualCoordinate[1] === coordinates[1]);
-		})[0];
-		
-		if (shipPresent) {
-			return true;
-		} 
-	}
-	
-	return false;
+function checkForShip(player, coordinates) {
+  for (let i = 0; i < player.ships.length; i += 1) {
+    const ship = player.ships[i];
+
+    const shipPresent = ship.locations.filter(actualCoordinate => (
+      (actualCoordinate[0] === coordinates[0]) && (actualCoordinate[1] === coordinates[1])
+    ))[0];
+
+    if (shipPresent) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
-function damageShip (ship, coordinates) {
-	ship.damage.push(coordinates);
+function damageShip(ship, coordinates) {
+  ship.damage.push(coordinates);
 }
 
 module.exports.checkForShip = checkForShip;
-module.exports.damageShip = damageShip; 
+module.exports.damageShip = damageShip;
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("hello world");
+const { checkForShip, damageShip } = require("./game_logic/ship_methods");
+
+checkForShip([0, 1]);
+damageShip({ locations: [] }, [0, 0]);
